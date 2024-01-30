bcc3b3f2fddd8c659f6cec6c6f96a6741a23191a
Evgeniy Tokarev <archjohn@gmail.com>
2024-01-30 03:10:07 +0200

    add a min example of non-supported capture phase keyboard events use.

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 # nixos-unstable. Find the current hash at https://status.nixos.org/.
 # Check https://nodejs.org/en/download for the current npm release.
-{ nixpkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/91a00709aebb3602f172a0bf47ba1ef013e34835.tar.gz") {config.allowUnfree = true;}
+{ nixpkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/56911ef3403a9318b7621ce745f5452fb9ef6867.tar.gz") {config.allowUnfree = true;}
 }:
 
 let
@@ -8,12 +8,12 @@ let
     nixpkgs.docker
     nixpkgs.docker-compose
     nixpkgs.git
-    nixpkgs.jdk
+    nixpkgs.adoptopenjdk-hotspot-bin-11
     nixpkgs.nodejs_20
     nixpkgs.openssh
-    nixpkgs.sbt
     nixpkgs.scala
     nixpkgs.which
+    (nixpkgs.sbt.override { jre = nixpkgs.adoptopenjdk-hotspot-bin-11; })
   ];
 
 in
--- src/main/scala/testvite/Main.scala
+++ src/main/scala/testvite/Main.scala
@@ -6,11 +6,53 @@ import org.scalajs.dom
 import slinky.core._
 import slinky.web.ReactDOM
 import slinky.web.html._
+import typings.react.reactStrings.alert
+import org.scalajs.dom.HTMLInputElement
+import org.scalajs.dom.Event
+import org.scalajs.dom.HTMLButtonElement
+import org.scalajs.dom.HTMLDivElement
+import slinky.web.SyntheticMouseEvent
 
 object Main {
+
+  def onClickHandlerCapturing(event: SyntheticMouseEvent[HTMLDivElement]): Unit = {
+    dom.window.alert("handleClick capturing phase")
+  }
+
+  def onClickHandlerTarget(event: SyntheticEvent[HTMLButtonElement, Event]): Unit = {
+    dom.window.alert("handleClick target phase")
+  }
+
+  def onClickHandlerBubbling(event: SyntheticMouseEvent[HTMLDivElement]): Unit = {
+    dom.window.alert("handleClick bubbling phase")
+  }
+
   def main(args: Array[String]): Unit = {
     ReactDOM.render(
-      h1("Hello, world!"),
+      div(className := "App")(
+        h1("3 phases of event propagation example"),
+        div(onClick := (event => onClickHandlerCapturing(event)))(
+          div(onClick := (event => onClickHandlerBubbling(event)))(
+            div(onClick := (event => onClickHandlerBubbling(event)))(
+              button(onClick := (event => onClickHandlerTarget(event)))("Click Me!")
+            )
+          )
+        ),
+        h2("keyboard events"),
+        ul(
+          li(
+            input(onKeyDown := (event => { println("onKeyDown") })),
+            input(onKeyPress := (event => { println("onKeyPress") })),
+            input(onKeyUp := (event => { println("onKeyUp") }))
+          ),
+          /// Following doesn't work but it should. ///
+          li(
+            input(onKeyUpCapture := (event => { println("onKeyUpCapture") })),
+            input(onKeyPressCapture := (event => { println("onKeyPressCapture") })),
+            input(onKeyDownCapture := (event => { println("onKeyDownCapture") }))
+          )
+        )
+      ),
       dom.document.getElementById("app")
     )
   }
